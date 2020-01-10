4bee7b8516074e9d267ff8ab6b0ba874bcb56602
WolfElkan <wolf.elkan@gmail.com>
2020-01-10 12:03:43 -0500

    Clean up syntax

--- chess.cpp
+++ chess.cpp
@@ -24,6 +24,50 @@ const int BlackRook   = 13;
 const int BlackBishop = 14;
 const int BlackQueen  = 15;
 
+int orthogonal[8][2] = {
+	{ 0, 1}, // North
+	{ 1, 0}, // East
+	{ 0,-1}, // South
+	{-1, 0}, // West
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+};
+
+int diagonal[8][2] = {
+	{ 1, 1}, // Northeast
+	{ 1,-1}, // Southeast
+	{-1,-1}, // Southwest
+	{-1, 1}, // Northwest
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+	{0 ,0 }, // placeholder
+};
+
+int octagonal[8][2] = {
+	{ 0, 1}, // North
+	{ 1, 1}, // Northeast
+	{ 1, 0}, // East
+	{ 1,-1}, // Southeast
+	{ 0,-1}, // South
+	{-1,-1}, // Southwest
+	{-1, 0}, // West
+	{-1, 1}, // Northwest
+};
+
+int theodorgonal[8][2] = { // all vectors with a magnitude of Theodorus' constant
+	{ 2, 1}, //  27 NNE
+	{ 1, 2}, //  63 ENE
+	{-1, 2}, // 117 ESE
+	{-2, 1}, // 153 SSE
+	{-2,-1}, // 207 SSW
+	{-1,-2}, // 243 WSW
+	{ 1,-2}, // 297 WNW
+	{ 2,-1}, // 333 NNW
+};
+
 int board[8][8];
 
 int court[8] = {5,2,6,7,3,6,2,5};
@@ -36,8 +80,6 @@ string String(char* a, int size){
     } 
     return s; 
 }
-
-// char Chars(string)
 
 char piece_symbol(int n){
 	char pieces[16] = {' ','P','N','K','X','R','B','Q','*','p','n','k','x','r','b','q'};
@@ -74,23 +116,6 @@ int printboard(int board[8][8], bool grid) {
 	}
 	return 0;
 }
-
-// class Piece {
-// public:
-// 	Piece(int rank, int file, bool color){
-// 		bool mov_range[8][8];
-// 		bool cap_range[8][8];
-// 	};
-	
-// };
-
-// class Pawn : public Piece {
-// public:
-// 	Pawn(int rank, int file, bool color){
-// 		Piece super(rank, file, color);
-// 		// cout << super.mov_range << endl;
-// 	}
-// };
 
 int parse_space(string space){
 	int rank = int(space[1]) - 49;
@@ -220,7 +245,7 @@ unsigned long long occupied(int board[8][8], bool color){
 	return board64(boolboard);
 }
 
-unsigned long long rangeP(int piece, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
+unsigned long long range_pawn(int piece, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
 	// En Passant not yet supported
 	int color = piece / 8;
 	int r1;
@@ -269,20 +294,20 @@ unsigned long long rangeP(int piece, int board[8][8], bool boolboard[8][8], int 
 	return board64(boolboard);
 }
 
-unsigned long long rangeN(int piece, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
-	int knight_move[8][2] = {
-		{ 2, 1},
-		{ 1, 2},
-		{-1, 2},
-		{-2, 1},
-		{-2,-1},
-		{-1,-2},
-		{ 1,-2},
-		{ 2,-1}
-	};
-	for (int i = 0; i < 8; i++){
-		int r1 = r0 + knight_move[i][0];
-		int f1 = f0 + knight_move[i][1];
+unsigned long long range_single(int piece, int dirs[8][2], int ndirs, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
+	// int knight_move[8][2] = {
+	// 	{ 2, 1},
+	// 	{ 1, 2},
+	// 	{-1, 2},
+	// 	{-2, 1},
+	// 	{-2,-1},
+	// 	{-1,-2},
+	// 	{ 1,-2},
+	// 	{ 2,-1}
+	// };
+	for (int i = 0; i < ndirs; i++){
+		int r1 = r0 + dirs[i][0];
+		int f1 = f0 + dirs[i][1];
 		if (r1 < 8 and r1 >= 0 and f1 < 8 and f1 >= 0){
 			int target = board[r1][f1];
 			if (move and (target == Empty)){
@@ -297,79 +322,53 @@ unsigned long long rangeN(int piece, int board[8][8], bool boolboard[8][8], int 
 	return board64(boolboard);
 }
 
-unsigned long long rangeK(int piece, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
-	int knight_move[8][2] = {
-		{ 0, 1},
-		{ 1, 1},
-		{ 1, 0},
-		{ 1,-1},
-		{ 0,-1},
-		{-1,-1},
-		{-1, 0},
-		{-1, 1},
-	};
-	for (int i = 0; i < 8; i++){
-		int r1 = r0 + knight_move[i][0];
-		int f1 = f0 + knight_move[i][1];
-		if (r1 < 8 and r1 >= 0 and f1 < 8 and f1 >= 0){
-			int target = board[r1][f1];
-			if (move and (target == Empty)){
-				boolboard[r1][f1] = 1;
-				cout << Piece_symbol(piece) << pack_space(r1,f1) << endl;
-			} else if (target and capture and (target/8)^(piece/8)){
-				boolboard[r1][f1] = 1;
-				cout << Piece_symbol(piece) << 'x' << pack_space(r1,f1) << endl;
-			}
-		}
-	}
-	return board64(boolboard);
-}
-
-unsigned long long rangeR(int piece, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
+unsigned long long range_long(int piece, int dirs[8][2], int ndirs, int board[8][8], bool boolboard[8][8], int r0, int f0, bool move, bool capture) {
 	
 	int r1;
 	int f1;
 	int target;
-	int rook_direction[4][2] = {
-		{ 0, 1}, // North
-		{ 1, 0}, // East
-		{ 0,-1}, // South
-		{-1, 0}, // West
-	};
-	for (int i = 0; i < 4; ++i) {
-		int rD = rook_direction[i][0];
-		int fD = rook_direction[i][1];
-		r1 = r0;
-		f1 = f0;
-		cout << rook_direction[i][0] << rook_direction[i][1] << endl;
-		while (r1 < 7 and r1 >= 1 and f1 < 7 and f1 >= 1 and target == Empty) {
+	for (int i = 0; i < ndirs; ++i) {
+		int rD = dirs[i][0];
+		int fD = dirs[i][1];
+		r1 = r0 + rD;
+		f1 = f0 + fD;
+		target = board[r1][f1];
+		// cout << dirs[i][0] << ' ' << dirs[i][1] << endl;
+		bool go = true;
+		while (go and r1 < 8 and r1 >= 0 and f1 < 8 and f1 >= 0) {
+			if (capture and (target/8)^(piece/8)){
+				boolboard[r1][f1] = target;
+				cout << Piece_symbol(piece) << 'x' << pack_space(r1,f1) << endl;
+				go = false;
+			}
+			if (target == Empty) {
+				boolboard[r1][f1] = move;
+				cout << Piece_symbol(piece) << pack_space(r1,f1) << endl;
+			} else {
+				go = false;
+			}
 			r1 += rD;
 			f1 += fD;
 			target = board[r1][f1];
-			if (target and capture and (target/8)^(piece/8)){
-				boolboard[r1][f1] = 1;
-				cout << Piece_symbol(piece) << 'x' << pack_space(r1,f1) << endl;
-			}
-			if (move and (target == Empty)) {
-				boolboard[r1][f1] = 1;
-				cout << Piece_symbol(piece) << pack_space(r1,f1) << endl;
-			} 
 		}
 	}
-
+	// for (int rank = 7; rank >=0; rank--) {
+	// 	for (int file = 0; file < 8; file++) {
+	// 		cout << boolboard[rank][file] << ' ';
+	// 	}
+	// 	cout << endl;
+	// }
+	// cout << endl;
 	return board64(boolboard);
 }
 
-unsigned long long range(int board[8][8], string space, bool move, bool capture) {
+unsigned long long range(int board[8][8], int r, int f, bool move, bool capture) {
 	bool boolboard[8][8];
 	for (int rank = 0; rank < 8; rank++){
 		for (int file = 0; file < 8; file++){
 			boolboard[rank][file] = 0;
 		}
 	}
-	int rf = parse_space(space);
-	int r = rf / 8;
-	int f = rf % 8;
 	int piece = board[r][f];
 	int color = piece / 8;
 	// cout << piece_symbol(piece) << endl;
@@ -380,35 +379,44 @@ unsigned long long range(int board[8][8], string space, bool move, bool capture)
 
 		case WhitePawn:
 		case BlackPawn:
-			return rangeP(piece, board, boolboard, r, f, move, capture);
+			return range_pawn(piece, board, boolboard, r, f, move, capture);
 			break;
 
 		case WhiteKnight:
 		case BlackKnight:
-			return rangeN(piece, board, boolboard, r, f, move, capture);
+			return range_single(piece, theodorgonal, 8, board, boolboard, r, f, move, capture);
 			break;
 
 		case WhiteKing:
 		case BlackKing:
-			return rangeK(piece, board, boolboard, r, f, move, capture);
+			return range_single(piece, octagonal, 8, board, boolboard, r, f, move, capture);
 			break;
 
 		case WhiteRook:
 		case BlackRook:
-			return rangeR(piece, board, boolboard, r, f, move, capture);
+			return range_long(piece, orthogonal, 4, board, boolboard, r, f, move, capture);
 			break;
 
 		case WhiteBishop:
 		case BlackBishop:
+			return range_long(piece, diagonal, 4, board, boolboard, r, f, move, capture);
 			break;
 
 		case WhiteQueen:
 		case BlackQueen:
+			return range_long(piece, octagonal, 8, board, boolboard, r, f, move, capture);
 			break;
 
 	}
 	return 0;
 }
+unsigned long long range(int board[8][8], string space, bool move, bool capture) {
+	int rf = parse_space(space);
+	int r = rf / 8;
+	int f = rf % 8;
+	return range(board, r, f, move, capture);}
+unsigned long long range(int board[8][8], int r, int f) {return range(board, r, f, 1, 1);}
+unsigned long long range(int board[8][8], string space) {return range(board, space, 1, 1);}
 
 int main(){
 	bool turn = 0;
@@ -419,12 +427,14 @@ int main(){
 		board[1][file] = 1;
 		board[0][file] = court[file];
 	}
-	board[4][3] = WhiteRook;
+	// board[4][3] = WhiteKnight; // d5
+	// board[3][7] = WhiteKing; // h4
 	bool go = true;
-		printboard(board,1);
-		string space;
-		cin >> space;
-		cout << range(board,space,1,1) << endl;
+		// printboard(board,1);
+		// string space;
+		// space = "d5";
+		// cin >> space;
+		// cout << range(board,space) << endl;
 		// for (int rank = 0; rank < 8; ++rank) {
 		// 	for (int file = 0; file < 8; ++file) {
 		// 		space = pack_space(rank,file);
@@ -435,26 +445,34 @@ int main(){
 		// 	}
 		// }
 
-	// while (go) {
-	// 	printboard(board,1);
-	// 	// setboard(board,0);
-	// 	// printboard(board,0);
-	// 	cout << turn_number << " ";
-	// 	if (turn == 0) {
-	// 		cout << "White";
-	// 	}
-	// 	if (turn == 1) {
-	// 		cout << "Black";
-	// 	}
-	// 	cout << ": ";
-	// 	string move = "";
-	// 	cin >> move;
-	// 	int x = make_move(board, turn, move);
-	// 	turn_number += x * turn;
-	// 	turn = turn ^ x;
-	// 	if (move == "quit"){
-	// 		go = false;
-	// 	}
-	// }
+	while (go) {
+		printboard(board,1);
+		// setboard(board,0);
+		// printboard(board,0);
+		for (int rank = 7; rank >= 0; rank--) {
+			for (int file = 0; file < 8; file++) {
+				int piece = board[rank][file];
+				if (piece and piece/8 == turn) {
+					range(board, rank, file);
+				}
+			}
+		}
+		cout << turn_number << " ";
+		if (turn == 0) {
+			cout << "White";
+		}
+		if (turn == 1) {
+			cout << "Black";
+		}
+		cout << ": ";
+		string move = "";
+		cin >> move;
+		int x = make_move(board, turn, move);
+		turn_number += x * turn;
+		turn = turn ^ x;
+		if (move == "quit"){
+			go = false;
+		}
+	}
 	return 0;
 }
